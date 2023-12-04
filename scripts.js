d8d9ade26fe7e96ec8345a2c8ab17aa625e9cf3a
Ana-Rafaela <anarafaelaa1904@gmail.com>
2023-12-04 18:10:45 -0300

    subindo projeto final

---
+++ scripts.js
@@ -0,0 +1,24 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+
+let header = document.getElementById('header')
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY >= 200){
+        header.style.background = '#030313'
+    } else {
+        header.style.background = 'transparent'
+    }
+
+})
